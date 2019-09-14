5dd280075b9d2fe40327d58db204a57a50e9e2e3
sreebose123 <srebose1@gmail.com>
2019-09-14 19:56:18 +0530

    @Test annotations is done

--- src/test/java/com/testng/programs/MyTestNgTest.java
+++ src/test/java/com/testng/programs/MyTestNgTest.java
@@ -1,5 +1,40 @@
 package com.testng.programs;
 
+import org.testng.annotations.Test;
+
 public class MyTestNgTest {
 
+	@Test
+	public void openBrowser() {
+	System.out.println("open browser");
+		
+   }
+	
+
+	@Test
+	public void openURL() {
+	System.out.println("open url");
+	
+   }
+	
+	
+	@Test
+	public void searchProduct() {
+	System.out.println("search iphone");
+	
+   }
+	
+	@Test
+	public void verifyProduct() {
+		System.out.println("verify product");
+	}
+	
+	
+	@Test
+	public void closeBrowser() {
+		System.out.println("close browser");
+	}
+	
+	
+	
 }
